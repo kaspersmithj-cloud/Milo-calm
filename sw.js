09880f1452f727117ee1bfa488e17f46611cd393
kaspersmithj-cloud <kaspersmithj@gmail.com>
2026-07-07 01:04:50 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="milo-trainer-pro-v3-1";
-const FILES=["./","index.html","styles.css","app.js","programme.js","manifest.json","sw.js"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
